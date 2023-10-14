fd97fc969ba285dc3cffb6d21b375d1b646a396c
Leechee403 <Leechee403@Leechee403.com>
2023-10-14 12:03:27 +0800

    :sparkles: finish interceptor and close filter

--- src/main/java/com/leechee/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/leechee/interceptor/LoginCheckInterceptor.java
@@ -1,44 +1,34 @@
-package com.leechee.filter;
+package com.leechee.interceptor;
 
-import java.io.IOException;
-
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.alibaba.fastjson.JSONObject;
 import com.leechee.pojo.Result;
 import com.leechee.utils.JwtUtils;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-// import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-// @WebFilter(urlPatterns = "/*")
-public class LoginCheckFilter implements Filter{
+@Component
+public class LoginCheckInterceptor implements HandlerInterceptor{
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse resp = (HttpServletResponse) response;
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler)
+            throws Exception {
 
         // 1. 获取url
         String url = req.getRequestURL().toString();
         log.info("请求的url为: {}", url);
 
-        // 2. 判断请求的url是否包含login，若包含则放行
-        if (url.contains("login")) {
-            log.info("登录操作，放行");
-            chain.doFilter(request, response);
-            return;
-        }
+        // // 2. 判断请求的url是否包含login，若包含则放行 使用拦截器已经在配置中省略了login，此处不再需要额外判断
+        // if (url.contains("login")) {
+        //     log.info("登录操作，放行");
+        //     return true;
+        // }
 
         // 3. 获取请求头的令牌 token
         String jwt = req.getHeader("token");
@@ -49,7 +39,7 @@ public class LoginCheckFilter implements Filter{
             Result error = Result.error("NOT_LOGIN");
             String notLogin = JSONObject.toJSONString(error);
             resp.getWriter().write(notLogin);
-            return;
+            return false;
         }
 
         // 5. 校验 jwt 令牌，不合法则返回 未登录 结果
@@ -61,13 +51,12 @@ public class LoginCheckFilter implements Filter{
             Result error = Result.error("NOT_LOGIN");
             String notLogin = JSONObject.toJSONString(error);
             resp.getWriter().write(notLogin);
-            return;
+            return false;
         }
 
         // 6. 放行
         log.info("令牌检验通过，放行");
-        chain.doFilter(request, response);
-
+        return true;
     }
-    
+
 }
